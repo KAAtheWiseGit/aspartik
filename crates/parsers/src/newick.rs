ff10265868b7b553277fe88b2f16cde066fb8c3e
Andrej Kolčin <self@kaathewise.net>
2024-10-26 20:02:06 +0300

    [parsers] newick: implement a parser

--- crates/parsers/src/newick.rs
+++ crates/parsers/src/newick.rs
@@ -10,11 +10,45 @@ pub fn parse_tree<S: AsRef<str>>(tree: S) -> Tree {
 	todo!()
 }
 
-fn parse_node<'a>(
-	s: &'a str,
-	tree: &mut Tree,
-	parent: Option<usize>,
-) -> &'a str {
+fn name(s: &str) -> (&str, &str) {
+	if s.starts_with('"') {
+		// quoted
+		// TODO: error on malformed unclosed quotes
+		// +1 because it skips the first quote
+		let end = s[1..].find('"').unwrap() + 1;
+
+		(&s[1..end], &s[end + 1..].trim_start())
+	} else {
+		// bare
+		let end = s.find([',', ';', ')', ':']).unwrap_or(s.len());
+
+		(&s[..end].trim(), &s[end..].trim())
+	}
+}
+
+fn attributes(s: &str) -> (&str, &str) {
+	// TODO: unclosed attributes
+	// XXX: can attributes have nested brackets in them?
+	let Some(end) = s.find(']') else {
+		return ("", s);
+	};
+
+	(&s[1..end], &s[end + 1..].trim_start())
+}
+
+fn distance(s: &str) -> (Option<f64>, &str) {
+	let end = s
+		.find(|ch: char| {
+			!ch.is_ascii_digit()
+				&& ch != '.' && ch != 'e' && ch != '-'
+				&& ch != '+'
+		})
+		.unwrap_or(s.len());
+
+	(str::parse(&s[..end]).ok(), &s[end..].trim_start())
+}
+
+fn node<'a>(s: &'a str, tree: &mut Tree, parent: Option<usize>) -> &'a str {
 	let mut s = s.trim();
 
 	let this_idx = tree.push(Node::new(None, None, None, "".to_owned()));
@@ -22,69 +56,29 @@ fn parse_node<'a>(
 	if s.starts_with('(') {
 		s = &s[1..];
 		while !s.starts_with(')') && s != "" {
-			s = parse_node(s, tree, Some(this_idx));
+			s = node(s, tree, Some(this_idx));
 		}
 		if s.starts_with(')') {
 			s = &s[1..];
 		}
 	}
 
-	dbg!(s);
+	let (name, s) = name(s);
+	let s = s.trim_start_matches(':').trim();
+	let (attributes, s) = attributes(s);
+	let (distance, s) = distance(s);
 
-	// Doesn't work on nodes without a distance
-	let name: Option<String> = if s.starts_with('"') {
-		// Quoted name
+	tree.set(
+		Node::new(
+			Some(name.to_owned()),
+			distance,
+			parent,
+			attributes.to_owned(),
+		),
+		this_idx,
+	);
 
-		// TODO: error on malformed unclosed quotes
-		let end = s[1..].find('"').unwrap();
-		let name_s = &s[1..end];
-		s = &s[end..];
-		Some(name_s.trim_matches('"').to_owned())
-	} else if s.starts_with(':') {
-		// Anonymous node
-		s = &s[1..];
-		None
-	} else {
-		// Bare name
-		if let Some((bare_name, rest)) = s.split_once(':') {
-			// With distance
-			s = rest;
-			Some(bare_name.trim().to_owned())
-		} else {
-			// No distance
-			s = "";
-			None
-		}
-	};
-
-	s = s.trim();
-	let attributes = if s.starts_with("[") {
-		// TODO: error on malformed unclosed attributes
-		let (attributes, rest) = s.split_once(']').unwrap();
-		s = rest;
-
-		attributes.to_owned()
-	} else {
-		"".to_owned()
-	};
-
-	s = s.trim();
-	let end = s
-		.char_indices()
-		.take_while(|(i, ch)| ch.is_ascii_alphanumeric() || *ch == '.')
-		.last()
-		// End should not be inclusive
-		.map(|(i, _)| i + 1)
-		.unwrap_or(0);
-	let distance = str::parse(&s[..end]).ok();
-	s = &s[end..];
-
-	let this_node = tree
-		.set(Node::new(name, distance, parent, attributes), this_idx);
-
-	s = s.trim().trim_start_matches(',').trim();
-
-	s
+	s.trim_start_matches([',']).trim()
 }
 
 #[cfg(test)]
@@ -93,10 +87,9 @@ mod tests {
 
 	#[test]
 	fn p() {
-		let s = "(A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F";
+		let s = "(:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F:0.0;";
 		let mut tree = Tree::empty();
-		parse_node(s, &mut tree, None);
-
+		node(s, &mut tree, None);
 		println!("{:#?}", tree);
 	}
 }
