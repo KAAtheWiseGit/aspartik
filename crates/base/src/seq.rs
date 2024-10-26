e43d5c530662b636ea592a5f894a98edbdcdd7e6
Andrej Kolčin <self@kaathewise.net>
2024-10-26 13:39:43 +0300

    [base] seq: add a `complement` method

--- crates/base/src/seq.rs
+++ crates/base/src/seq.rs
@@ -1,4 +1,4 @@
-use crate::bases::Base;
+use crate::bases::{Base, DnaNucleoBase};
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct Seq<T: Base> {
@@ -9,6 +9,16 @@ impl<T: Base> Seq<T> {
 	pub fn reverse(&self) -> Self {
 		let mut out = self.clone();
 		out.value.reverse();
+		out
+	}
+}
+
+impl Seq<DnaNucleoBase> {
+	fn complement(&self) -> Self {
+		let mut out = self.clone();
+		for base in out.value.iter_mut() {
+			*base = base.complement();
+		}
 		out
 	}
 }
