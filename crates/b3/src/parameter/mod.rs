2e53b233873075070da08e8c3b7e569eac266285
Andrej Kolčin <self@kaathewise.net>
2024-11-05 18:40:22 +0300

    [b3] parameter: create parameter types

--- crates/b3/src/parameter/mod.rs
+++ crates/b3/src/parameter/mod.rs
@@ -1,3 +1,14 @@
 // XXX: should this include the tree?
-#[derive(Clone)]
-pub enum Parameter {}
+#[derive(Debug, Clone)]
+pub enum Parameter {
+	Real(Param<f64>),
+	Integer(Param<i64>),
+	Boolean(Param<bool>),
+}
+
+#[derive(Debug, Clone)]
+pub struct Param<T: PartialOrd> {
+	values: Vec<T>,
+	min: Option<T>,
+	max: Option<T>,
+}
