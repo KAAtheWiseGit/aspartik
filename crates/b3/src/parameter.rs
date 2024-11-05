43757303acd44ce42a6a7d2b9c19b73f6f4e07d7
Andrej Kolčin <self@kaathewise.net>
2024-11-05 19:13:26 +0300

    [b3] parameter: add `is_valid` method

--- crates/b3/src/parameter.rs
+++ crates/b3/src/parameter.rs
@@ -7,8 +7,28 @@ pub enum Parameter {
 }
 
 #[derive(Debug, Clone)]
-pub struct Param<T: PartialOrd> {
-	values: Vec<T>,
-	min: Option<T>,
-	max: Option<T>,
+pub struct Param<T: Copy + PartialOrd> {
+	pub values: Vec<T>,
+	pub min: Option<T>,
+	pub max: Option<T>,
+}
+
+impl<T: Copy + PartialOrd> Param<T> {
+	/// Returns `false` if any value inside the parameter is out of bounds.
+	pub fn is_valid(&self) -> bool {
+		self.values
+			.iter()
+			.map(|val| {
+				let lower =
+					self.min.map(|min| min <= *val)
+						.unwrap_or(true);
+				let upper =
+					self.max.map(|max| *val <= max)
+						.unwrap_or(true);
+
+				upper && lower
+			})
+			// all items are true
+			.all(|x| x)
+	}
 }
