57ae7998cd9798f2c39b2061195d1e47b82e57e6
Andrej Kolčin <self@kaathewise.net>
2024-11-07 17:24:20 +0300

    [b3] operator: a stub for the list of operators

--- crates/b3/src/operator/mod.rs
+++ crates/b3/src/operator/mod.rs
@@ -2,6 +2,28 @@ use std::collections::HashMap;
 
 use crate::{parameter::Parameter, state::State};
 
+// Operators:
+// - [ ] Parameter operators
+//   - [ ] "Bit flips" for boolean parameters
+//   - [ ] Uniform selection in parameter bounds
+//   - [ ] Swap operator for exchanging dimension values
+//   - [ ] Regular and Bactrian versions:
+//     - [ ] Delta exchange
+//     - [ ] Random walk (+/- delta)
+//     - [ ] "Up/down": scale two parameters using the same coefficient
+// - [ ] Tree operators
+//   - [ ] Epoch scaling
+//   - [ ] Exchange: swap two branches, narrow and wide variations
+//   - [ ] Random node height, reconstructs the tree
+//   - [ ] Scaling
+//   - [ ] Uniform: move node height between parent and children
+//   - [ ] Subtree sliding: same as above, but the node can be swapped with
+//     parents
+//   - [ ] Leaf node moving and scaling
+//   - [ ] Wilson-Balding branch swapping move
+//   - [ ] Bactrian versions for all distribution dependent operators from the
+//     above
+
 #[derive(Debug, Clone, Copy)]
 pub enum Status {
 	Accept,
