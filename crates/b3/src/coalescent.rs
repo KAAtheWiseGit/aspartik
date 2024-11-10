b844092a1819c32705a791a6817166a3cf8a3271
Andrej Kolčin <self@kaathewise.net>
2024-11-10 13:32:32 +0300

    [b3] coalescent: create from a tree

--- crates/b3/src/coalescent.rs
+++ crates/b3/src/coalescent.rs
@@ -9,6 +9,44 @@ pub struct Coalescent {
 }
 
 impl Coalescent {
+	pub fn new<S>(
+		sequences: S,
+		substitution_model: Substitution,
+		nodes: &[f64],
+		edges: &[(usize, usize)],
+	) -> Self
+	where
+		S: IntoIterator<Item = DnaSeq>,
+	{
+		let sequences: Vec<DnaSeq> = sequences.into_iter().collect();
+		let mut columns = Vec::new();
+
+		for i in 0..sequences[0].len() {
+			let mut column = DnaSeq::new();
+			for sequence in &sequences {
+				column.push(sequence[i]);
+			}
+			columns.push(column);
+		}
+
+		let mut substitutions = Vec::new();
+		for (i, (left, right)) in edges.iter().enumerate() {
+			let left_distance = nodes[*left] - nodes[i];
+			let right_distance = nodes[*right] - nodes[i];
+
+			let left = (substitution_model * left_distance).exp();
+			let right = (substitution_model * right_distance).exp();
+
+			substitutions.push((left, right));
+		}
+
+		Self {
+			columns,
+			children: edges.into(),
+			substitutions,
+		}
+	}
+
 	pub fn likelihood(&self) -> f64 {
 		let mut out = 1.0;
 
@@ -65,8 +103,26 @@ mod test {
 	fn minimal() {
 		use base::substitution::dna::jukes_cantor;
 
-		let bases = DnaSeq::try_from("AAGCT").unwrap();
-
+		let seqs = vec![
+			DnaSeq::try_from("AAGCT").unwrap(),
+			DnaSeq::try_from("CAGCT").unwrap(),
+			DnaSeq::try_from("ATGCA").unwrap(),
+			DnaSeq::try_from("ATGCT").unwrap(),
+			DnaSeq::try_from("TAGCA").unwrap(),
+		];
 		let tree = vec![(2, 3), (0, 1), (5, 4), (6, 7)];
+		let distances =
+			vec![0.75, 0.60, 1.1, 0.9, 0.85, 0.8, 0.5, 0.7, 0.3];
+
+		let coalescent = Coalescent::new(
+			seqs,
+			jukes_cantor(),
+			&distances,
+			&tree,
+		);
+
+		let likelihood = coalescent.likelihood();
+
+		println!("{likelihood}");
 	}
 }
