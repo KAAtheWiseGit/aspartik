e6242e418345a8725fcdace0c031bac6fe514782
Andrej Kolčin <self@kaathewise.net>
2024-11-10 18:04:39 +0300

    [b3] coalescent: rewrite with `wide`
    Right now it's almost twice as slow.  My use of `as_array_ref` seems to
    be the culprit.

--- crates/b3/src/coalescent.rs
+++ crates/b3/src/coalescent.rs
@@ -1,20 +1,21 @@
-use glam::{DMat4, DVec4};
 use nalgebra::Matrix4;
+use wide::f64x4;
 
 use base::{seq::DnaSeq, DnaNucleoBase};
 
-type Substitution = Matrix4<f64>;
+type Row = f64x4;
+type Substitution = [f64x4; 4];
 
 pub struct Coalescent {
 	columns: Vec<DnaSeq>,
 	children: Vec<(usize, usize)>,
-	substitutions: Vec<(DMat4, DMat4)>,
+	substitutions: Vec<(Substitution, Substitution)>,
 }
 
 impl Coalescent {
 	pub fn new<S>(
 		sequences: S,
-		substitution_model: Substitution,
+		substitution_model: Matrix4<f64>,
 		nodes: &[f64],
 		edges: &[(usize, usize)],
 	) -> Self
@@ -44,7 +45,10 @@ impl Coalescent {
 			let left = (substitution_model * left_distance).exp();
 			let right = (substitution_model * right_distance).exp();
 
-			substitutions.push((left.into(), right.into()));
+			let left = to_sub(left);
+			let right = to_sub(right);
+
+			substitutions.push((left, right));
 		}
 
 		Self {
@@ -57,10 +61,8 @@ impl Coalescent {
 	pub fn likelihood(&self) -> f64 {
 		let mut out = 0.0;
 
-		const ANY: DVec4 = DVec4::new(0.25, 0.25, 0.25, 0.25);
-
 		let mut t = vec![
-			DVec4::ZERO;
+			f64x4::new([0.0, 0.0, 0.0, 0.0]);
 			self.columns.len() + self.children.len()
 		];
 
@@ -68,34 +70,74 @@ impl Coalescent {
 			for (i, base) in column.iter().enumerate() {
 				match base {
 					DnaNucleoBase::Adenine => {
-						t[i] = DVec4::X;
+						t[i] = f64x4::new([
+							1.0, 0.0, 0.0, 0.0,
+						]);
 					}
 					DnaNucleoBase::Cytosine => {
-						t[i] = DVec4::Y;
+						t[i] = f64x4::new([
+							0.0, 0.0, 1.0, 0.0,
+						]);
 					}
 					DnaNucleoBase::Guanine => {
-						t[i] = DVec4::Z;
+						t[i] = f64x4::new([
+							0.0, 0.0, 1.0, 0.0,
+						]);
 					}
 					DnaNucleoBase::Thymine => {
-						t[i] = DVec4::W;
+						t[i] = f64x4::new([
+							0.0, 0.0, 0.0, 1.0,
+						]);
 					}
-					_ => t[i] = ANY,
+					_ => {
+						t[i] = f64x4::new([
+							0.25, 0.25, 0.25, 0.25,
+						])
+					}
 				}
 			}
 
 			for i in 0..self.children.len() {
-				let left = self.substitutions[i].0
-					* t[self.children[i].0];
-				let right = self.substitutions[i].1
-					* t[self.children[i].1];
+				let left = multiply(
+					t[self.children[i].0],
+					self.substitutions[i].0,
+				);
+				let right = multiply(
+					t[self.children[i].1],
+					self.substitutions[i].1,
+				);
 
 				let parent = left * right;
 				t[i + column.len()] = parent;
 			}
 
-			out += t.last().unwrap().element_sum().ln();
+			out += t.last().unwrap().reduce_add().ln();
 		}
 
 		out
 	}
+}
+
+fn multiply(row: Row, sub: Substitution) -> f64x4 {
+	let a = sub[0] * row.as_array_ref()[0];
+	let c = sub[1] * row.as_array_ref()[1];
+	let g = sub[2] * row.as_array_ref()[2];
+	let t = sub[3] * row.as_array_ref()[3];
+
+	f64x4::new([
+		a.reduce_add(),
+		c.reduce_add(),
+		g.reduce_add(),
+		t.reduce_add(),
+	])
+}
+
+fn to_sub(sub: Matrix4<f64>) -> Substitution {
+	let [a, c, g, t]: &[[f64; 4]; 4] = sub.as_ref();
+	[
+		f64x4::new(*a),
+		f64x4::new(*c),
+		f64x4::new(*g),
+		f64x4::new(*t),
+	]
 }
