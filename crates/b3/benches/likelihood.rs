d79c1925cdd7f28dbd3b4ba0a608948d671bdaf1
Andrej Kolčin <self@kaathewise.net>
2024-11-12 14:35:51 +0300

    [b3] benches/likelihood: update to use the MCMC

--- crates/b3/benches/likelihood.rs
+++ crates/b3/benches/likelihood.rs
@@ -1,11 +1,15 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use b3::tree::*;
-use base::seq::DnaSeq;
+use b3::{
+	mcmc::{run, Config},
+	operator::{scheduler::TurnScheduler, NarrowExchange, Operator},
+	probability::Compound,
+	state::State,
+	tree::Tree,
+};
+use base::{seq::DnaSeq, substitution::dna::jukes_cantor};
 
 fn likelihood() {
-	use base::substitution::dna::jukes_cantor;
-
 	let seqs = vec![
 		DnaSeq::try_from("AAGCT".repeat(10).as_ref()).unwrap(),
 		DnaSeq::try_from("CAGCT".repeat(10).as_ref()).unwrap(),
@@ -13,24 +17,27 @@ fn likelihood() {
 		DnaSeq::try_from("ATGCT".repeat(10).as_ref()).unwrap(),
 		DnaSeq::try_from("TAGCA".repeat(10).as_ref()).unwrap(),
 	];
-	let tree = vec![(2, 3), (0, 1), (5, 4), (6, 7)];
-	let distances = vec![0.75, 0.60, 1.1, 0.9, 0.85, 0.8, 0.5, 0.7, 0.3];
+	let children = vec![(2, 3), (0, 1), (5, 4), (6, 7)];
+	let weights = vec![0.75, 0.60, 1.1, 0.9, 0.85, 0.8, 0.5, 0.7, 0.3];
+	let tree = Tree::new(seqs.clone(), jukes_cantor(), &weights, &children);
+	let mut state = State::new(tree);
+	let prior = Box::new(Compound::new([]));
 
-	for _ in 0..1_000 {
-		let tree = Tree::new(
-			seqs.clone(),
-			jukes_cantor(),
-			&distances,
-			&tree,
-		);
-		for _ in 0..1_000 {
-			tree.likelihood();
-		}
-	}
+	let operator: Box<dyn Operator> = Box::new(NarrowExchange());
+	let mut scheduler = TurnScheduler::new([operator]);
+
+	let config = Config {
+		burnin: 0,
+		length: 1_000_000,
+		state: 10_000,
+		trees: 10_000,
+	};
+
+	run(config, &mut state, prior, &mut scheduler);
 }
 
 fn bench(c: &mut Criterion) {
-	c.bench_function("likelihood", |b| b.iter(|| likelihood()));
+	c.bench_function("likelihood", |b| b.iter(likelihood));
 }
 
 criterion_group!(
