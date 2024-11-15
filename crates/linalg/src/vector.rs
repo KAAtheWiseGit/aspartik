4428b8ba54e462fa691c62e9465b59a6640f6bf1
Andrej Kolčin <self@kaathewise.net>
2024-11-15 11:33:42 +0300

    [linalg] vector: add methods and basic arithmetic

--- crates/linalg/src/vector.rs
+++ crates/linalg/src/vector.rs
@@ -1,42 +1,96 @@
-use std::{
-	mem::{self, MaybeUninit},
-	ops::{Add, AddAssign, Mul},
-};
+use std::ops::{Add, AddAssign, Index, IndexMut, Mul, MulAssign};
 
-pub struct Vector<T, const N: usize> {
+#[derive(Debug, Clone, Copy)]
+pub struct Vector<T: Copy, const N: usize> {
 	v: [T; N],
 }
 
-impl<T, const N: usize> From<[T; N]> for Vector<T, N> {
+impl<T: Copy, const N: usize> From<[T; N]> for Vector<T, N> {
 	fn from(value: [T; N]) -> Self {
 		Self { v: value }
 	}
 }
 
-impl<T, const N: usize> Add for Vector<T, N>
-where
-	T: Add + Copy,
-{
+impl<T: Copy, const N: usize> Index<usize> for Vector<T, N> {
+	type Output = T;
+
+	fn index(&self, index: usize) -> &Self::Output {
+		&self.v[index]
+	}
+}
+
+impl<T: Copy, const N: usize> IndexMut<usize> for Vector<T, N> {
+	fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+		&mut self.v[index]
+	}
+}
+
+impl<T: Copy + AddAssign, const N: usize> AddAssign for Vector<T, N> {
+	fn add_assign(&mut self, rhs: Self) {
+		for i in 0..N {
+			self[i] += rhs[i];
+		}
+	}
+}
+
+impl<T: Copy + AddAssign, const N: usize> Add for Vector<T, N> {
 	type Output = Self;
 
 	fn add(self, rhs: Self) -> Self::Output {
-		let mut out = [const { MaybeUninit::uninit() }; N];
+		let mut out = self;
+		out += rhs;
+		out
+	}
+}
 
-		#[allow(clippy::needless_range_loop)]
+impl<T: Copy + MulAssign, const N: usize> MulAssign for Vector<T, N> {
+	fn mul_assign(&mut self, rhs: Self) {
 		for i in 0..N {
-			out[i].write(self.v[i] + rhs.v[i]);
+			self[i] *= rhs[i];
 		}
+	}
+}
 
-		// SAFETY: for the integer/float types [T; N] has the same
-		// layout as [MaybeUninit<T>; N].  Verifying this for
-		// non-primitive types is a TODO.
-		unsafe { mem::transmute_copy::<_, [T; N]>(&out).into() }
+impl<T: Copy + MulAssign, const N: usize> Mul for Vector<T, N> {
+	type Output = Self;
+
+	fn mul(self, rhs: Self) -> Self::Output {
+		let mut out = self;
+		out *= rhs;
+		out
+	}
+}
+
+// Arithmetic-agnostic implementations.
+impl<T: Copy, const N: usize> Vector<T, N> {
+	pub fn len(&self) -> usize {
+		N
+	}
+}
+
+impl<T: Copy + AddAssign, const N: usize> Vector<T, N> {
+	pub fn sum(&self) -> T {
+		let mut out = self[0];
+		for i in 1..N {
+			out += self[i];
+		}
+		out
+	}
+}
+
+impl<T: Copy + MulAssign, const N: usize> Vector<T, N> {
+	pub fn product(&self) -> T {
+		let mut out = self[0];
+		for i in 1..N {
+			out *= self[i];
+		}
+		out
 	}
 }
 
 impl<T, const N: usize> Vector<T, N>
 where
-	T: Add + AddAssign + Mul<Output = T> + Copy,
+	T: Copy + AddAssign + Mul<Output = T>,
 {
 	pub fn dot(&self, other: &Vector<T, N>) -> T {
 		let mut out = self.v[0] * other.v[0];
