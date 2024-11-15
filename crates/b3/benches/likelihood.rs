f0d3695de0f7a219238066b16c9f8eae09ebff87
Andrej Kolčin <self@kaathewise.net>
2024-11-15 13:34:13 +0300

    [b3] benches/likelihood: bigger test data

--- crates/b3/benches/likelihood.rs
+++ crates/b3/benches/likelihood.rs
@@ -1,4 +1,5 @@
 use criterion::{criterion_group, criterion_main, Criterion};
+use rand::{rngs::SmallRng, seq::SliceRandom, SeedableRng};
 
 use b3::{
 	mcmc::{run, Config},
@@ -7,7 +8,43 @@ use b3::{
 	state::State,
 	tree::Tree,
 };
-use base::{seq::DnaSeq, substitution::dna::jukes_cantor};
+use base::{seq::DnaSeq, substitution::dna::jukes_cantor, DnaNucleoBase};
+
+fn data() -> (Vec<DnaSeq>, Vec<(usize, usize)>) {
+	let mut rng = SmallRng::seed_from_u64(4);
+
+	let bases = [
+		DnaNucleoBase::Adenine,
+		DnaNucleoBase::Cytosine,
+		DnaNucleoBase::Guanine,
+		DnaNucleoBase::Thymine,
+	];
+	let mut seqs: Vec<DnaSeq> = vec![];
+	for _ in 0..512 {
+		let mut seq = DnaSeq::new();
+		for _ in 0..1024 {
+			let base = bases.choose(&mut rng).unwrap();
+			seq.push(*base);
+		}
+		seqs.push(seq);
+	}
+
+	let mut children = vec![];
+	let mut prev = 0;
+	for level in 0..8 {
+		let size = 2_usize.pow(8 - level);
+
+		for i in 0..size {
+			let left = prev + 2 * i;
+			let right = prev + 2 * i + 1;
+			children.push((left, right));
+		}
+
+		prev += size * 2;
+	}
+
+	(seqs, children)
+}
 
 fn likelihood() {
 	let seqs = vec![
