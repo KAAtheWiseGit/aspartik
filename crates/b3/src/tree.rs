b39a3861e6dd4989d81c53087ced964d09f7ae0a
Andrej Kolčin <self@kaathewise.net>
2024-11-15 13:47:29 +0300

    [b3] tree: decouple from likelihood calculations

--- crates/b3/src/tree.rs
+++ crates/b3/src/tree.rs
@@ -4,30 +4,19 @@ use rand::{
 	Rng,
 };
 use serde_json::{json, Value as Json};
-use wide::f64x4;
 
-use std::collections::{HashSet, VecDeque};
-
-use crate::operator::TreeEdit;
-use base::{seq::DnaSeq, DnaNucleoBase};
-
-type Row = f64x4;
-type Substitution = [f64x4; 4];
+use crate::{likelihood::DnaLikelihood, operator::TreeEdit};
+use base::seq::DnaSeq;
 
 const ROOT: usize = usize::MAX;
 
 pub struct Tree {
-	/// Leaf node DNA sequences.
-	columns: Vec<DnaSeq>,
 	/// Tuples of left and right children indices of the internal nodes.
 	children: Vec<(usize, usize)>,
 	parents: Vec<usize>,
-	probabilities: Vec<Vec<Row>>,
 	weights: Vec<f64>,
 
-	/// Substitution matrices for edges, specified in `children`.
-	substitutions: Vec<(Substitution, Substitution)>,
-	model: Matrix4<f64>,
+	likelihoods: Vec<DnaLikelihood>,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -52,71 +41,58 @@ pub struct Internal(usize);
 pub struct Leaf(usize);
 
 impl Tree {
-	pub fn new<S>(
-		sequences: S,
-		substitution_model: Matrix4<f64>,
+	pub fn new(
+		sequences: &[DnaSeq],
+		// TODO
+		_substitution_model: Matrix4<f64>,
 		weights: &[f64],
-		edges: &[(usize, usize)],
-	) -> Self
-	where
-		S: IntoIterator<Item = DnaSeq>,
-	{
-		let sequences: Vec<DnaSeq> = sequences.into_iter().collect();
+		children: &[(usize, usize)],
+	) -> Self {
 		let mut columns = Vec::new();
 		for i in 0..sequences[0].len() {
 			let mut column = DnaSeq::new();
-			for sequence in &sequences {
+			for sequence in sequences {
 				column.push(sequence[i]);
 			}
 			columns.push(column);
 		}
 
 		let num_leaves = columns[0].len();
-		let num_internals = edges.len();
 		let num_nodes = weights.len();
 
 		let mut parents = vec![ROOT; num_nodes];
-		for (i, (left, right)) in edges.iter().enumerate() {
+		for (i, (left, right)) in children.iter().enumerate() {
 			parents[*left] = i + num_leaves;
 			parents[*right] = i + num_leaves;
 		}
 
-		let zero_row = f64x4::new([0.0, 0.0, 0.0, 0.0]);
-
-		let substitutions =
-			vec![([zero_row; 4], [zero_row; 4]); num_internals];
-
-		let probabilities = vec![vec![zero_row; num_nodes]; num_leaves];
-
-		let mut out = Self {
-			columns,
-			children: edges.into(),
+		Self {
+			children: children.to_vec(),
 			parents,
-			probabilities,
-			weights: weights.into(),
-			substitutions,
-			model: substitution_model,
-		};
+			weights: weights.to_vec(),
 
-		out.update_substitutions(out.num_leaves()..out.num_nodes());
-		out.update_leaf_probabilites();
-		out.update_internal_probabilities(
-			out.num_leaves()..out.num_nodes(),
-		);
-
-		out
+			likelihoods: vec![],
+		}
 	}
 
-	pub fn num_leaves(&self) -> usize {
-		self.columns[0].len()
+	pub fn likelihood(&self) -> f64 {
+		self.likelihoods.iter().map(|l| l.likelihood()).sum()
+	}
+
+	pub fn update_with(&mut self, _edit: TreeEdit) -> TreeEdit {
+		todo!()
 	}
 
 	pub fn num_internals(&self) -> usize {
 		self.children.len()
 	}
 
+	pub fn num_leaves(&self) -> usize {
+		self.num_internals() + 1
+	}
+
 	pub fn num_nodes(&self) -> usize {
-		self.num_leaves() + self.num_internals()
+		self.weights.len()
 	}
 
 	pub fn is_leaf<N: Into<Node>>(&self, node: N) -> bool {
@@ -203,211 +179,10 @@ impl Tree {
 		(0..self.num_leaves()).map(Leaf)
 	}
 
-	pub fn likelihood(&self) -> f64 {
-		self.probabilities
-			.iter()
-			.map(|p| p.last().unwrap().reduce_add().ln())
-			.sum()
-	}
-
-	fn update_substitutions<I>(&mut self, nodes: I)
-	where
-		I: IntoIterator<Item = usize>,
-	{
-		let num_leaves = self.num_leaves();
-
-		for i in nodes.into_iter().map(|i| i - num_leaves) {
-			let (left, right) = self.children[i];
-
-			let left_distance =
-				self.weights[left] - self.weights[i];
-			let right_distance =
-				self.weights[right] - self.weights[i];
-
-			let left = (self.model * left_distance).exp();
-			let right = (self.model * right_distance).exp();
-
-			let left = to_sub(left);
-			let right = to_sub(right);
-
-			self.substitutions[i] = (left, right);
-		}
-	}
-
-	fn update_leaf_probabilites(&mut self) {
-		for (column, probability) in
-			self.columns.iter().zip(&mut self.probabilities)
-		{
-			for (i, base) in column.iter().enumerate() {
-				probability[i] = to_row(base);
-			}
-		}
-	}
-
-	fn update_internal_probabilities<I>(&mut self, nodes: I)
-	where
-		I: IntoIterator<Item = usize> + Clone,
-	{
-		let num_leaves = self.num_leaves();
-
-		for probability in &mut self.probabilities {
-			// This should be fast for ranges and a cheap copy for
-			// slice iterators.
-			for i in nodes
-				.clone()
-				.into_iter()
-				.map(|i| i - num_leaves)
-			{
-				let left = multiply(
-					probability[self.children[i].0],
-					self.substitutions[i].0,
-				);
-				let right = multiply(
-					probability[self.children[i].1],
-					self.substitutions[i].1,
-				);
-				probability[i + num_leaves] = left * right;
-			}
-		}
-	}
-
-	pub fn update_with(&mut self, edit: TreeEdit) -> TreeEdit {
-		let mut old_weights = Vec::with_capacity(edit.weights.len());
-		for (node, weight) in edit.weights.iter().copied() {
-			old_weights.push((node, self.weights[node.0]));
-			self.weights[node.0] = weight;
-		}
-
-		/// Swap the children of parent of `x`
-		macro_rules! swap {
-			($parent_x:ident, $x:ident, $y:ident) => {
-				let num_leaves = self.num_leaves();
-				if let Some(parent) = $parent_x {
-					if self.children_of(parent).0 == $y {
-						self.children[parent.0
-							- num_leaves]
-							.0 = $y.0;
-					} else {
-						self.children[parent.0
-							- num_leaves]
-							.1 = $y.0;
-					}
-				}
-			};
-		}
-
-		for (a, b) in edit.parents.iter().copied() {
-			let parent_a = self.parent_of(a);
-			let parent_b = self.parent_of(b);
-
-			swap!(parent_a, a, b);
-			swap!(parent_b, b, a);
-
-			self.parents[a.0] =
-				parent_b.map(|p| p.0).unwrap_or(ROOT);
-			self.parents[b.0] =
-				parent_a.map(|p| p.0).unwrap_or(ROOT);
-		}
-
-		self.update_affected(
-			edit.weights.iter().map(|(n, _)| n).copied(),
-		);
-		self.update_affected(
-			edit.parents.iter().map(|(n, _)| n).copied(),
-		);
-
-		TreeEdit {
-			weights: old_weights,
-			parents: edit.parents,
-		}
-	}
-
-	fn update_affected<I>(&mut self, nodes: I)
-	where
-		I: IntoIterator<Item = Node>,
-	{
-		let mut deq = VecDeque::<usize>::new();
-		let mut set = HashSet::<usize>::new();
-
-		for node in nodes {
-			let mut curr = node.0;
-			let mut chain = Vec::new();
-
-			// Walk up from the starting nodes until the root, stop
-			// when we encounter a node we have already walked.
-			while !set.contains(&curr) && curr != ROOT {
-				set.insert(curr);
-
-				// If the node is internal, add it to the
-				// current chain.
-				if curr >= self.num_leaves() {
-					chain.push(curr);
-				}
-
-				curr = self.parents[curr];
-			}
-
-			// Prepend the chain to the deque.  The first chain will
-			// insert the root node and walk backwards.  All of the
-			// rest will also go in the front, ensuring that
-			// children always go befor their parents.
-			while let Some(val) = chain.pop() {
-				deq.push_front(val);
-			}
-		}
-
-		let slice = deq.make_contiguous();
-		self.update_substitutions(slice.iter().copied());
-		self.update_internal_probabilities(slice.iter().copied());
-	}
-
 	pub fn serialize(&self) -> Json {
 		json!({
 			"children": self.children,
 			"weights": self.weights,
 		})
 	}
-}
-
-fn to_row(base: &DnaNucleoBase) -> Row {
-	match base {
-		DnaNucleoBase::Adenine => [1.0, 0.0, 0.0, 0.0],
-		DnaNucleoBase::Cytosine => [0.0, 0.0, 1.0, 0.0],
-		DnaNucleoBase::Guanine => [0.0, 0.0, 1.0, 0.0],
-		DnaNucleoBase::Thymine => [0.0, 0.0, 0.0, 1.0],
-		// TODO: other types
-		_ => [0.25, 0.25, 0.25, 0.25],
-	}
-	.into()
-}
-
-#[inline(always)]
-fn multiply(row: Row, sub: Substitution) -> f64x4 {
-	let a = sub[0] * row.as_array_ref()[0];
-	let c = sub[1] * row.as_array_ref()[1];
-	let g = sub[2] * row.as_array_ref()[2];
-	let t = sub[3] * row.as_array_ref()[3];
-
-	/// The reason we need this function is because the `wide` `reduce_add`
-	/// method is marked `inline` and, unlike the `glam` methods, it seems
-	/// that rustc doesn't inline it.  So, this naive implementation
-	/// compiles to a faster binary.
-	#[inline(always)]
-	fn reduce(vec: f64x4) -> f64 {
-		let vec = vec.as_array_ref();
-
-		vec[0] + vec[1] + vec[2] + vec[3]
-	}
-
-	f64x4::new([reduce(a), reduce(c), reduce(g), reduce(t)])
-}
-
-fn to_sub(sub: Matrix4<f64>) -> Substitution {
-	let [a, c, g, t]: &[[f64; 4]; 4] = sub.as_ref();
-	[
-		f64x4::new(*a),
-		f64x4::new(*c),
-		f64x4::new(*g),
-		f64x4::new(*t),
-	]
 }
