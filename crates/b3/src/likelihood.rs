ed2b1e53c5ca763a853b72d1548b1c6561083b37
Andrej Kolčin <self@kaathewise.net>
2024-11-15 15:36:03 +0300

    [b3] likelihood: implement updates

--- crates/b3/src/likelihood.rs
+++ crates/b3/src/likelihood.rs
@@ -5,7 +5,7 @@ use base::substitution::Model;
 pub struct Likelihood<M: Model> {
 	sites: Vec<Vec<M::Item>>,
 	model: M,
-	substitutions: Vec<(M::Substitution, M::Substitution)>,
+	substitutions: Vec<M::Substitution>,
 	probabilities: Vec<Vec<M::Row>>,
 }
 
@@ -15,13 +15,8 @@ impl<M: Model> Likelihood<M> {
 		S: IntoIterator<Item = Vec<M::Item>>,
 	{
 		let sites: Vec<_> = sites.into_iter().collect();
-		let substitutions = vec![
-			(
-				M::Substitution::default(),
-				M::Substitution::default()
-			);
-			sites[0].len()
-		];
+		let substitutions =
+			vec![M::Substitution::default(); sites[0].len()];
 
 		let mut probabilities =
 			vec![
@@ -43,12 +38,31 @@ impl<M: Model> Likelihood<M> {
 		}
 	}
 
-	pub fn update_substitutions(&mut self, /* nodes, distances, l/r */) {
-		todo!()
+	pub fn update_substitutions(
+		&mut self,
+		edges: &[usize],
+		distances: &[f64],
+	) {
+		for (edge, distance) in edges.iter().zip(distances) {
+			self.substitutions[*edge] =
+				self.model.substitution(*distance);
+		}
 	}
 
-	pub fn update_probabilities(&mut self /* nodes, tree */) {
-		todo!()
+	pub fn update_probabilities(
+		&mut self,
+		nodes: &[usize],
+		children: &[(usize, usize)],
+	) {
+		for probability in &mut self.probabilities {
+			for (i, (left, right)) in nodes.iter().zip(children) {
+				let left = self.substitutions[i * 2]
+					* probability[*left];
+				let right = self.substitutions[i * 2 + 1]
+					* probability[*right];
+				probability[*i] = left * right;
+			}
+		}
 	}
 
 	pub fn likelihood(&self) -> f64 {
