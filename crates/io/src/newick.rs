d5ce74c4dcd05b209871a9bb9b3b6e271da9c8ca
Andrej Kolčin <self@kaathewise.net>
2024-11-18 17:03:49 +0300

    [io] newick: start a rewrite
    Uses `nom` to avoid reimplementing all of the string matching manually.

--- crates/io/src/newick.rs
+++ crates/io/src/newick.rs
@@ -1,105 +1,136 @@
-#![allow(unused)]
+#![allow(dead_code)]
 
-use std::collections::HashMap;
+use nom::{
+	branch::alt,
+	bytes::{tag, take_till},
+	character::complete::{char, multispace0, none_of},
+	combinator::value,
+	multi::fold_many0,
+	number::complete::float,
+	sequence::delimited,
+	IResult, Parser,
+};
+use petgraph::graph::DiGraph;
 
-use base::newick::{Node, Tree};
-
-pub fn parse_tree<S: AsRef<str>>(tree: S) -> Tree {
-	let _s = tree.as_ref();
-
-	todo!()
+struct Node {
+	name: String,
+	attributes: String,
+	length: f64,
 }
 
-fn name(s: &str) -> (&str, &str) {
-	if let Some(s) = s.strip_prefix('"') {
-		// quoted
-		let end = s.find('"').unwrap();
+impl Node {
+	pub fn name(&self) -> &str {
+		&self.name
+	}
 
-		(&s[..end], s[end..].trim_start())
-	} else {
-		// bare
-		let end = s.find([',', ';', ')', ':']).unwrap_or(s.len());
-
-		(s[..end].trim(), s[end..].trim())
+	pub fn length(&self) -> f64 {
+		self.length
 	}
 }
 
-fn attributes(s: &str) -> (&str, &str) {
-	// TODO: unclosed attributes
-	// XXX: can attributes have nested brackets in them?
-	let Some(end) = s.find(']') else {
-		return ("", s);
+pub struct Tree {
+	graph: DiGraph<Node, ()>,
+}
+
+macro_rules! ws {
+	($f:expr) => {
+		|s| delimited(multispace0, $f, multispace0).parse(s)
 	};
-
-	(&s[1..end], s[end + 1..].trim_start())
 }
 
-fn distance(s: &str) -> (Option<f64>, &str) {
-	let end = s
-		.find(|ch: char| {
-			!ch.is_ascii_digit()
-				&& ch != '.' && ch != 'e' && ch != '-'
-				&& ch != '+'
-		})
-		.unwrap_or(s.len());
+fn bare(input: &str) -> IResult<&str, String> {
+	let ch = none_of(" \t\n,;[\"");
 
-	(str::parse(&s[..end]).ok(), s[end..].trim_start())
+	fold_many0(ch, String::new, |mut string, mut fragment| {
+		if fragment == '_' {
+			fragment = ' ';
+		}
+		string.push(fragment);
+		string
+	})
+	.parse(input)
 }
 
-const EMPTY: String = String::new();
+fn quoted(input: &str) -> IResult<&str, String> {
+	let ch = alt((none_of("\"\\"), value('"', tag("\\\""))));
 
-fn node<'a>(s: &'a str, tree: &mut Tree, parent: Option<usize>) -> &'a str {
-	let mut s = s.trim();
+	let build = fold_many0(ch, String::new, |mut string, fragment| {
+		string.push(fragment);
+		string
+	});
 
-	// Insert a placeholder node before we finish parsing all of the
-	// descendants
-	let this_idx = tree.push(Node::new(EMPTY, None, None, EMPTY));
+	delimited(char('"'), build, char('"')).parse(input)
+}
 
-	if s.starts_with('(') {
-		s = &s[1..];
-		while !s.starts_with(')') && !s.is_empty() {
-			s = node(s, tree, Some(this_idx));
-		}
-		if s.starts_with(')') {
-			s = &s[1..];
-		}
-	}
+fn name(input: &str) -> IResult<&str, String> {
+	// Try `quoted` first
+	alt((quoted, bare)).parse(input)
+}
 
-	let (name, s) = name(s);
-	let s = s.trim_start_matches(':').trim();
-	let (attributes, s) = attributes(s);
-	let (distance, s) = distance(s);
+fn comment(input: &str) -> IResult<&str, String> {
+	// XXX: can Newick contain nested brackets?
+	delimited(char('['), take_till(|c| c != ']'), char(']'))
+		.parse(input)
+		.map(|(rest, s)| (rest, s.to_owned()))
+}
 
-	tree.set(
-		Node::new(
-			name.to_owned(),
-			distance,
-			parent,
-			attributes.to_owned(),
-		),
-		this_idx,
-	);
+fn length(input: &str) -> IResult<&str, f64> {
+	float(input).map(|(rest, f)| (rest, f as f64))
+}
 
-	s.trim_start_matches([',']).trim()
+fn body(input: &str) -> IResult<&str, Node> {
+	let (rest, name) = ws!(name)(input)?;
+	let (rest, attributes) = ws!(comment)(rest)?;
+	let (rest, length) = ws!(length)(rest)?;
+
+	Ok((
+		rest,
+		Node {
+			name,
+			attributes,
+			length,
+		},
+	))
 }
 
 #[cfg(test)]
-mod tests {
+mod test {
 	use super::*;
 
-	#[test]
-	fn p() {
-		let s = "(:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F:0.0;";
-		let mut tree = Tree::empty();
-		node(s, &mut tree, None);
-		println!("{:#?}", tree);
+	macro_rules! check {
+		($tests:ident, $parser:expr) => {
+			for (string, answer) in $tests {
+				let result = $parser(string);
+				assert!(result.is_ok());
+				assert_eq!(
+					(answer.0, answer.1.to_owned()),
+					result.unwrap()
+				);
+			}
+		};
 	}
 
 	#[test]
-	fn tree() {
-		let s = "((11:78.51463972926828,(((9:7.687822300343535,(8:1.2997671677365752,7:1.2997671677365752):6.38805513260696):2.5517113635399182,10:10.239533663883453):47.83909385190117,(((4:10.303956314457084,(3:7.014862584373447,2:7.014862584373447):3.2890937300836365):10.890052001978896,5:21.19400831643598):8.165283167190701,6:29.35929148362668):28.719336032157944):20.436012213483657):16.847636009595632,(1:74.35882993398783,12:74.35882993398783):21.00344580487608):0.0;";
-		let mut tree = Tree::empty();
-		node(s, &mut tree, None);
-		println!("{:#?}", tree);
+	fn test_name() {
+		let bare_tests = [
+			("bareIdent", ("", "bareIdent")),
+			("rest of it", (" of it", "rest")),
+			("under_score", ("", "under score")),
+			("__and_trailing___", ("", "  and trailing   ")),
+		];
+		check!(bare_tests, bare);
+
+		let quoted_tests = [
+			(r#""a quoted string""#, ("", "a quoted string")),
+			(
+				r#""quoted with" trailing"#,
+				(" trailing", "quoted with"),
+			),
+			(r#""esc\"ape""#, ("", "esc\"ape")),
+		];
+		check!(quoted_tests, quoted);
+
+		check!(bare_tests, name);
+		check!(quoted_tests, name);
 	}
 }
