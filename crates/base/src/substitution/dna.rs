f300b97c9f2cd1908c2c13c89968eb0ee53105f2
Andrej Kolčin <self@kaathewise.net>
2024-11-18 13:25:02 +0300

    [base] substitution/dna: separate models by diagonalization

--- crates/base/src/substitution/dna.rs
+++ crates/base/src/substitution/dna.rs
@@ -7,9 +7,24 @@ type Row = Vector<f64, 4>;
 type Substitution = RowMatrix<f64, 4, 4>;
 
 pub struct Dna4Substitution {
+	inner: Matrix,
+}
+
+// It's fine, the other variant will be big too
+#[allow(clippy::large_enum_variant)]
+enum Matrix {
+	Diagonalizable(Diagonalizable),
+	Defective(Defective),
+}
+
+struct Diagonalizable {
 	p: Substitution,
 	p_rev: Substitution,
 	d: Substitution,
+}
+
+struct Defective {
+	// TODO
 }
 
 impl Dna4Substitution {
@@ -33,7 +48,22 @@ impl Dna4Substitution {
 			[0.0, 0.0, 0.0, 0.0],
 		]);
 
-		Self { p, p_rev, d }
+		Self {
+			inner: Matrix::Diagonalizable(Diagonalizable {
+				p,
+				p_rev,
+				d,
+			}),
+		}
+	}
+}
+
+impl Dna4Substitution {
+	fn as_diagonalizable(&self) -> Option<&Diagonalizable> {
+		match &self.inner {
+			Matrix::Diagonalizable(d) => Some(d),
+			_ => None,
+		}
 	}
 }
 
@@ -59,11 +89,15 @@ impl Model for Dna4Substitution {
 	}
 
 	fn substitution(&self, distance: f64) -> Substitution {
-		let mut e_d = self.d;
-		for i in 0..4 {
-			e_d[(i, i)] = f64::exp(distance * e_d[(i, i)]);
-		}
+		if let Some(diag) = self.as_diagonalizable() {
+			let mut e_d = diag.d;
+			for i in 0..4 {
+				e_d[(i, i)] = f64::exp(distance * e_d[(i, i)]);
+			}
 
-		self.p * e_d * self.p_rev
+			diag.p * e_d * diag.p_rev
+		} else {
+			todo!()
+		}
 	}
 }
