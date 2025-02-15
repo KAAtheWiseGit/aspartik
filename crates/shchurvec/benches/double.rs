ee1a4064010637949dae26d66edff093275bf175
Andrej Kolčin <self@kaathewise.net>
2025-02-15 15:42:46 +0300

    [shchurvec] benches: use `divan` instead of `criterion`

--- crates/shchurvec/benches/double.rs
+++ crates/shchurvec/benches/double.rs
@@ -1,4 +1,5 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use divan::Bencher;
+
 use rand::{rngs::SmallRng, Rng, SeedableRng};
 
 use std::hint::black_box;
@@ -12,45 +13,42 @@ fn data(length: usize) -> ShchurVec<Row> {
 	let mut rng = SmallRng::seed_from_u64(4);
 
 	for _ in 0..length {
-		out.push(rng.gen());
+		out.push(rng.random());
 	}
 
 	out
 }
 
-fn edit(v: &mut ShchurVec<Row>) {
-	let mut rng = SmallRng::seed_from_u64(4);
+#[inline(always)]
+fn edit(v: &mut ShchurVec<[f64; 4]>, rng: &mut SmallRng) {
 	let num = v.len() / 10;
 
 	for _ in 0..num {
-		let i = rng.gen_range(0..v.len());
-		v.set(i, rng.gen());
+		let i = rng.random_range(0..v.len());
+		v.set(i, rng.random());
 	}
-
-	v.accept();
 }
 
-fn bench(c: &mut Criterion) {
-	let mut v = black_box(data(100_000));
-
-	c.bench_function("edit_accept", |b| {
-		b.iter(|| {
-			edit(&mut v);
-			v.accept()
-		});
-	});
-
-	c.bench_function("edit_reject", |b| {
-		b.iter(|| {
-			edit(&mut v);
-			v.reject()
-		});
-	});
+#[divan::bench]
+fn edit_accept(bencher: Bencher) {
+	bencher.with_inputs(|| black_box(data(100_000)))
+		.bench_values(|mut v| {
+			let mut rng = SmallRng::seed_from_u64(4);
+			edit(&mut v, &mut rng);
+			v.accept();
+		})
 }
 
-criterion_group!(
-	name = double;
-	config = Criterion::default();
-	targets = bench
-);
-criterion_main!(double);
+#[divan::bench]
+fn edit_reject(bencher: Bencher) {
+	bencher.with_inputs(|| black_box(data(100_000)))
+		.bench_values(|mut v| {
+			let mut rng = SmallRng::seed_from_u64(4);
+			edit(&mut v, &mut rng);
+			v.reject();
+		})
+}
+
+fn main() {
+	divan::main();
+}
