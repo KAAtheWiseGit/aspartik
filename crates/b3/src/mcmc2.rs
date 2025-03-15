8853dbc6bfcb22f0d853a05ca675c1d25832d4a6
Andrej Kolčin <self@kaathewise.net>
2025-03-15 14:06:28 +0300

    [b3] mcmc2: get the operator proposals

--- crates/b3/src/mcmc2.rs
+++ crates/b3/src/mcmc2.rs
@@ -2,27 +2,55 @@ use anyhow::Result;
 use pyo3::prelude::*;
 use rand::Rng as _;
 
-use crate::{prior::PyPrior, state::PyState};
+use crate::{
+	operator::{Proposal, PyOperator, WeightedScheduler},
+	prior::PyPrior,
+	state::PyState,
+};
 
 #[pyfunction]
-pub fn run(length: usize, state: PyState, priors: Vec<PyPrior>) -> Result<()> {
-	for i in 0..length {
-		// XXX: should we just acquire the lock for the entire duration
-		// of `run`?
-		Python::with_gil(|py| -> Result<()> {
-			step(py, &state, &priors)
-		})?;
-		// TODO: logging
-	}
+pub fn run(
+	length: usize,
+	state: PyState,
+	priors: Vec<PyPrior>,
+	operators: Vec<PyOperator>,
+) -> Result<()> {
+	// We acquire the lock for the full duration of the program so that we
+	// don't spend time locking and unlocking
+	Python::with_gil(|py| -> Result<()> {
+		let mut scheduler = WeightedScheduler::new(py, operators)?;
 
-	Ok(())
+		for _ in 0..length {
+			step(py, &state, &priors, &mut scheduler)?;
+			// TODO: logging
+		}
+		Ok(())
+	})
 }
 
-fn step(py: Python, state: &PyState, priors: &[PyPrior]) -> Result<()> {
+fn step(
+	py: Python,
+	state: &PyState,
+	priors: &[PyPrior],
+	scheduler: &mut WeightedScheduler,
+) -> Result<()> {
 	// TODO: select operator
+	let operator =
+		scheduler.select_operator(&mut state.inner().rng.inner());
 
-	// TODO: propose
-	let hastings = 0.0;
+	let hastings = match operator.propose(py, state)? {
+		Proposal::Accept() => {
+			// TODO: propose
+
+			accept(state)?;
+			return Ok(());
+		}
+		Proposal::Reject() => {
+			return Ok(());
+		}
+		Proposal::Hastings(ratio) => ratio,
+	};
+	// TODO: apply proposal
 
 	// TODO: get tree likelihood
 	let likelihood = 0.0;
