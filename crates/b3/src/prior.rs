69aff2e907a9c8e32b35a7eff6551a40a36b68bb
Andrej Kolčin <self@kaathewise.net>
2025-04-02 15:01:18 +0300

    [b3] prior: cleanup

--- crates/b3/src/prior.rs
+++ crates/b3/src/prior.rs
@@ -4,28 +4,19 @@ use pyo3::{conversion::FromPyObject, exceptions::PyTypeError, types::PyTuple};
 
 use crate::state::PyState;
 
-// TODO: name
 pub struct PyPrior {
+	/// INVARIANT: the type has a `probability` method
 	inner: PyObject,
 }
 
 impl PyPrior {
-	pub fn new(obj: PyObject) -> Self {
-		Self { inner: obj }
-	}
-}
-
-impl PyPrior {
 	pub fn probability(&self, py: Python, state: &PyState) -> Result<f64> {
-		// TODO: name logging
 		let args = PyTuple::new(py, [state.clone()])?;
 		let out = self
 			.inner
 			.bind(py)
 			.call_method1("probability", args)?
 			.extract::<f64>()?;
-		let out: f64 = Ok::<f64, PyErr>(out)?;
-
 		Ok(out)
 	}
 }
