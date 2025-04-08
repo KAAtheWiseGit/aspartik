e014ce20ab6b785418be2c07427bf79a73243a0b
Andrej Kolčin <self@kaathewise.net>
2025-04-08 16:11:07 +0300

    [b3] python: simplify re-exports

--- crates/b3/python/b3/__init__.py
+++ crates/b3/python/b3/__init__.py
@@ -1,16 +1,13 @@
 # ruff: noqa: F405
 
 from ._b3_rust_impl import *  # noqa: F403
-
-from . import priors
-from . import substitutions
-from . import operators
+from . import operators, priors, substitutions
 
 
 __doc__ = _b3_rust_impl.__doc__
-if hasattr(_b3_rust_impl, "__all__"):
-    __all__ = _b3_rust_impl.__all__
 
-__all__.append("operators")
-__all__.append("priors")
-__all__.append("substitutions")
+__all__ = []
+if hasattr(_b3_rust_impl, "__all__"):
+    __all__.append(_b3_rust_impl.__all__)
+
+__all__.append(["operators", "priors", "substitutions"])
