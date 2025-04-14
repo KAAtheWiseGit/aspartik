b784c230a7c65f5778aec4279a0392db40a3d786
Andrej Kolčin <self@kaathewise.net>
2025-04-14 15:35:37 +0300

    [linalg] tests/eigen: add matrix round-tripping test

--- crates/linalg/tests/eigen.rs
+++ crates/linalg/tests/eigen.rs
@@ -1,19 +1,37 @@
-use linalg::RowMatrix;
+use linalg::{RowMatrix, Vector};
 
 const EPSILON: f64 = 1e-15;
 
-macro_rules! assert_vector_eq {
-	($left:expr, $right:expr) => {
-		for i in 0..$right.len() {
-			let diff = ($left[i] - $right[i]).abs();
+fn assert_vector_eq<const N: usize>(
+	left: Vector<f64, N>,
+	right: Vector<f64, N>,
+) {
+	for i in 0..N {
+		let diff = (left[i] - right[i]).abs();
+		if diff > EPSILON {
+			panic!(
+				"Vectors are not equal:\n\t{:?}\n\t{:?}\n",
+				left, right
+			);
+		}
+	}
+}
+
+fn assert_matrix_eq<const N: usize, const M: usize>(
+	left: RowMatrix<f64, N, M>,
+	right: RowMatrix<f64, N, M>,
+) {
+	for i in 0..N {
+		for j in 0..M {
+			let diff = (left[i][j] - right[i][j]).abs();
 			if diff > EPSILON {
 				panic!(
-					"Vectors are not equal:\n\t{:?}\n\t{:?}\n",
-					$left, $right
+					"Matrices are not equal:\n\t{:?}\n\t{:?}\n",
+					left, right
 				);
 			}
 		}
-	};
+	}
 }
 
 #[test]
@@ -21,13 +39,31 @@ fn basic_eigenvalues() {
 	let m = RowMatrix::from([[4.0, 1.0], [2.0, -1.0]]);
 	assert_eq!([4.372281323269014, -1.3722813232690143], m.eigenvalues());
 
-	let one_third = 1.0 / 3.0;
 	let m = RowMatrix::from([
-		[1.0, -one_third, -one_third, -one_third],
-		[-one_third, 1.0, -one_third, -one_third],
-		[-one_third, -one_third, 1.0, -one_third],
-		[-one_third, -one_third, -one_third, 1.0],
+		[1.0, -1.0 / 3.0, -1.0 / 3.0, -1.0 / 3.0],
+		[-1.0 / 3.0, 1.0, -1.0 / 3.0, -1.0 / 3.0],
+		[-1.0 / 3.0, -1.0 / 3.0, 1.0, -1.0 / 3.0],
+		[-1.0 / 3.0, -1.0 / 3.0, -1.0 / 3.0, 1.0],
 	]);
-	let ff = 4.0 / 3.0;
-	assert_vector_eq!([0.0, ff, ff, ff], m.eigenvalues());
+	assert_vector_eq(
+		[0.0, 4.0 / 3.0, 4.0 / 3.0, 4.0 / 3.0].into(),
+		m.eigenvalues(),
+	);
+}
+
+#[test]
+fn roundtrip() {
+	let jc = RowMatrix::from([
+		[-1.0, 1.0 / 3.0, 1.0 / 3.0, 1.0 / 3.0],
+		[1.0 / 3.0, -1.0, 1.0 / 3.0, 1.0 / 3.0],
+		[1.0 / 3.0, 1.0 / 3.0, -1.0, 1.0 / 3.0],
+		[1.0 / 3.0, 1.0 / 3.0, 1.0 / 3.0, -1.0],
+	]);
+
+	let diag = RowMatrix::from_diagonal(jc.eigenvalues());
+	let eigenvectors = jc.eigenvectors();
+	let inverse = eigenvectors.inverse();
+
+	assert_matrix_eq(jc, inverse * diag * eigenvectors);
+	assert_matrix_eq(jc * 0.1, inverse * (diag * 0.1) * eigenvectors);
 }
